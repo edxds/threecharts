5803551062aa72241475786f9fcf2fa49044c3d1
Eduardo Souza <edux404@gmail.com>
2020-04-04 18:51:22 -0300

    [core] Introduce TransitionFadeThrough component

---
+++ src/app/components/TransitionFadeThrough.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Transition } from 'react-transition-group';
+
+const TransitionContainer = styled.div<{ state: string }>`
+  position: absolute;
+  inset: 0;
+
+  transition: all 200ms ease-in-out;
+  opacity: ${(p) => (p.state === 'entered' ? 1 : 0)};
+  transform: scale(${(p) => (p.state === 'entering' ? 0.92 : 1)});
+`;
+
+export const TransitionFadeThrough: React.FC = ({ children, ...rest }) => (
+  <Transition timeout={200} {...rest}>
+    {(state) => <TransitionContainer state={state}>{children}</TransitionContainer>}
+  </Transition>
+);
