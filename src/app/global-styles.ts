b4d41030eabd723a64bea49de5252c9e8cc8b182
Eduardo Souza <edux404@gmail.com>
2020-04-04 20:23:17 -0300

    [GlobalStyles] Move font import to HTML

--- src/app/global-styles.ts
+++ src/app/global-styles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap');
-
   html, body {
     width: 100%;
     height: 100%;
