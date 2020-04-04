41d75463a90bf471d4aae934ffbf01eec344121b
Eduardo Souza <edux404@gmail.com>
2020-04-04 19:52:35 -0300

    [MuiTheme] Update typography values

--- src/app/mui-theme.ts
+++ src/app/mui-theme.ts
@@ -4,6 +4,30 @@ export const MuiTheme = createMuiTheme({
   typography: {
     fontFamily: `'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;`,
+    h1: {
+      fontSize: '2.25rem',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: '1.825rem',
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: '1.425rem',
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: '1.125rem',
+      fontWeight: 500,
+    },
   },
   palette: {
     primary: {
