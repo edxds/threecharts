6740df33e062944b238a37f2a60711b7422fc5ee
Eduardo Souza <edux404@gmail.com>
2020-04-04 17:43:01 -0300

    [GlobalStyle] Add CSS to make root elements fill the whole screen

--- src/app/global-styles.ts
+++ src/app/global-styles.ts
@@ -3,13 +3,23 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap');
 
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
   body {
     margin: 0;
-    
+
     font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  #root, #app {
+    width: 100%;
+    height: 100%;
   }
 `;
