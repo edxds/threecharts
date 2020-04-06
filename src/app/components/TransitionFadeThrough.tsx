e5bb3f8e2042a82b5c16747220dac321ae40e632
Eduardo Souza <edux404@gmail.com>
2020-04-06 01:56:06 -0300

    [TransitionFadeThrough] Make TransitionContainer fill whole space

--- src/app/components/TransitionFadeThrough.tsx
+++ src/app/components/TransitionFadeThrough.tsx
@@ -4,6 +4,10 @@ import { Transition } from 'react-transition-group';
 
 const TransitionContainer = styled.div<{ state: string }>`
   position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 
   transition: all 200ms ease-in-out;
   opacity: ${(p) => (p.state === 'entered' ? 1 : 0)};
