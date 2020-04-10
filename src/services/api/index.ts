fa613345d03d009c1b47e7b03be5b69f9baa007e
Eduardo Souza <edux404@gmail.com>
2020-04-10 20:40:53 -0300

    [api] Export API methods as api object

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,8 @@
+import * as api from './api-methods';
+
 export { instance as defaultClient } from './api-client';
 
 export * from './api-error';
 export * from './api-result';
 
-export * as api from './api-methods';
+export { api };
