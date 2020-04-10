d531f6fc026528efeb79eb6e501db10f310a832d
Eduardo Souza <edux404@gmail.com>
2020-04-10 20:40:40 -0300

    [api] Add authorization methods

--- src/services/api/api-methods.ts
+++ src/services/api/api-methods.ts
@@ -1,9 +1,26 @@
 import { AxiosInstance, AxiosError } from 'axios';
 
 import { UserWeeksDto } from '@threecharts/models/UserWeeksDto';
+import { AuthorizationUrlDto } from '@threecharts/models/AuthorizationUrlDto';
+import { UserDto } from '@threecharts/models/UserDto';
 
 import { ApiResult } from './api-result';
 import { handleApiError } from './handle-api-error';
+
+export const getAuthorizationUrl = (
+  instance: AxiosInstance,
+  callbackUrl: string,
+): Promise<ApiResult<AuthorizationUrlDto>> =>
+  instance
+    .get(`/authorization/url?callback=${callbackUrl}`)
+    .then((response) => ApiResult.ok(response.data as AuthorizationUrlDto))
+    .catch((error) => handleApiError(error as AxiosError));
+
+export const tryAuthorize = (instance: AxiosInstance, token: string): Promise<ApiResult<UserDto>> =>
+  instance
+    .post(`/authorization/authorize`, { token })
+    .then((response) => ApiResult.ok(response.data as UserDto))
+    .catch((error) => handleApiError(error as AxiosError));
 
 export const getWeeks = (
   instance: AxiosInstance,
