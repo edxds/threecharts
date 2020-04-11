043a6197218dbe6f0d453de0480496152a6a7be0
Eduardo Souza <edux404@gmail.com>
2020-04-11 04:02:38 -0300

    [Home] Add week fetching logic with Redux

--- src/app/features/home/Home.tsx
+++ src/app/features/home/Home.tsx
@@ -1,21 +1,32 @@
-import React, { useState } from 'react';
-import { Typography } from '@material-ui/core';
+import 'styled-components/macro';
+
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRouteMatch, Redirect, Switch, Route, useLocation } from 'react-router-dom';
 import { TransitionGroup } from 'react-transition-group';
+import { Typography, CircularProgress, Collapse, Button } from '@material-ui/core';
+import { format, parseJSON } from 'date-fns';
 
+import { AppState } from '@threecharts/app/redux/store';
+import { defaultClient } from '@threecharts/services/api';
 import { useResizeObserver } from '@threecharts/hooks/useResizeObserver';
 import { useScrollDirection } from '@threecharts/hooks/useScrollDirection';
 import { TransitionSharedContainer } from '@threecharts/app/components/TransitionSharedContainer';
 import { TransitionFadeThrough } from '@threecharts/app/components/TransitionFadeThrough';
+import { ColoredMessageBox } from '@threecharts/app/components/ColoredMessageBox';
+import { Stack } from '@threecharts/app/components/Stack';
 
 import { WeeksPanel } from '../weeks';
+import { getWeeks } from '../weeks/slice';
 
 import { HomeBottomNavigation } from './HomeBottomNavigation';
 import { Styled } from './Home.styles';
 
 export const Home = () => {
   const [isWeeksPanelOpen, setIsWeeksPanelOpen] = useState(false);
-  const [selectedWeek, setSelectedWeek] = useState<number | null>(null);
+  const [selectedWeekId, setSelectedWeekId] = useState<number | null>(null);
+
+  const { status: weekStatus, weeks } = useSelector((state: AppState) => state.weeks);
 
   const [scrollHandler, scrollDirection] = useScrollDirection();
   const [navBarRef, navBarEntry] = useResizeObserver();
@@ -23,8 +34,23 @@ export const Home = () => {
   const location = useLocation();
   const match = useRouteMatch('/');
 
+  const dispatch = useDispatch();
+
+  const fetchWeeks = useCallback(async () => {
+    dispatch(getWeeks(defaultClient, 2));
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchWeeks();
+  }, [fetchWeeks]);
+
+  const didWeeksFail = weekStatus === 'rejected';
+  const areWeeksLoading = weekStatus === 'pending';
+
   const navBarHeight = navBarEntry?.contentRect.height ?? 56;
   const isNavBarHidden = scrollDirection === 'DOWN';
+
+  const selectedWeek = weeks.find((week) => week.id === selectedWeekId);
 
   // Set "default" tab to tracks by redirecting to /tracks
   // if we are at the base URL. Kinda hacky, no?
@@ -85,10 +111,47 @@ export const Home = () => {
           isOpen={isWeeksPanelOpen}
           onOpen={() => setIsWeeksPanelOpen(true)}
           onClose={() => setIsWeeksPanelOpen(false)}
-          value={selectedWeek}
-          onChange={setSelectedWeek}
+          onRefresh={fetchWeeks}
+          isLoading={areWeeksLoading}
+          value={selectedWeekId}
+          onChange={setSelectedWeekId}
+          title={selectedWeek && `Semana ${selectedWeek.weekNumber}`}
           ContainerProps={{ elevation: 2 }}
-        ></WeeksPanel>
+        >
+          <Collapse in={areWeeksLoading || didWeeksFail}>
+            {areWeeksLoading && (
+              <Stack direction="row" justify="center" align="center" padding="16px 0" spacing={16}>
+                <CircularProgress color="inherit" size={16} />
+                <Typography color="textPrimary" variant="body1">
+                  Carregando semanas...
+                </Typography>
+              </Stack>
+            )}
+            {didWeeksFail && (
+              <ColoredMessageBox
+                message="Algo deu errado. Verifique sua conexão e tente novamente."
+                css="margin: 16px"
+              >
+                <Stack direction="row" justify="flex-end" padding="32px 0 0">
+                  <Button color="primary" variant="contained" onClick={fetchWeeks}>
+                    Tentar Novamente
+                  </Button>
+                </Stack>
+              </ColoredMessageBox>
+            )}
+          </Collapse>
+          {[...weeks] // You can't run sort() on Redux arrays!
+            .sort((a, b) => b.weekNumber - a.weekNumber)
+            .map((week) => ({
+              id: week.id,
+              weekNumber: week.weekNumber,
+              from: format(parseJSON(week.from), 'dd/MM/yyyy'),
+              to: format(parseJSON(week.to), 'dd/MM/yyyy'),
+            }))
+            .map((week) => (
+              <WeeksPanel.Item key={week.id} data={week} />
+            ))}
+        </WeeksPanel>
       </Styled.HomeWeeksPanelContainer>
       <Styled.HomeNavigationBar
         ref={navBarRef}
