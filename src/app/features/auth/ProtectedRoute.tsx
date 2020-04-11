3996862656d2286c235f7df6eb1f921ebcccf3dd
Eduardo Souza <edux404@gmail.com>
2020-04-11 04:01:06 -0300

    [core] Introduce ProtectedRoute component

---
+++ src/app/features/auth/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { AppState } from '@threecharts/app/redux/store';
+
+type RouteProps = React.ComponentProps<typeof Route>;
+
+export const ProtectedRoute: React.FC<RouteProps> = (props) => {
+  const { status } = useSelector((state: AppState) => state.auth);
+  const location = useLocation<{ modalBackground: Location }>();
+
+  if (status === 'rejected') {
+    if (!location.state?.modalBackground && location.pathname !== '/login') {
+      return <Redirect to={{ pathname: '/login', state: { modalBackground: location } }} />;
+    }
+  }
+
+  return <Route {...props} />;
+};
