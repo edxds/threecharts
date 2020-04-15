d79824fc3f1e2a39192c5b577d8888471f3f9fae
Eduardo Souza <edux404@gmail.com>
2020-04-14 23:53:00 -0300

    [User] Add default preferences functionality

--- src/app/features/user/slice.ts
+++ src/app/features/user/slice.ts
@@ -7,15 +7,21 @@ import { AppThunk } from '@threecharts/app/redux/store';
 
 import { authorizeRejected } from '../auth/slice';
 
-type UserStatusType = 'idle' | 'pending' | 'resolved' | 'rejected';
+type PromiseStatusType = 'idle' | 'pending' | 'resolved' | 'rejected';
 
 type UserState = {
-  status: UserStatusType;
+  status: PromiseStatusType;
+  preferences: {
+    status: PromiseStatusType;
+  };
   currentUser: UserDto | null;
 };
 
 const initialState: UserState = {
   status: 'idle',
+  preferences: {
+    status: 'idle',
+  },
   currentUser: null,
 };
 
@@ -33,6 +39,16 @@ const userSlice = createSlice({
     userRejected(state, _: Action) {
       state.status = 'rejected';
       state.currentUser = null;
+    },
+    preferencesPending(state, _: Action) {
+      state.preferences.status = 'pending';
+    },
+    preferencesResolved(state, action: PayloadAction<UserDto>) {
+      state.preferences.status = 'pending';
+      state.currentUser = action.payload;
+    },
+    preferencesRejected(state, _: Action) {
+      state.preferences.status = 'rejected';
     },
   },
 });
@@ -53,8 +69,40 @@ export const getUserDetails = (instance: AxiosInstance): AppThunk => async (disp
   }
 
   const user = result.value();
+  if (user.ianaTimezone === null) {
+    dispatch(putUserDefaultPreferences(instance));
+  }
+
   dispatch(userResolved(user));
 };
 
-export const { userPending, userResolved, userRejected } = userSlice.actions;
+export const putUserDefaultPreferences = (instance: AxiosInstance): AppThunk => async (
+  dispatch,
+) => {
+  dispatch(preferencesPending());
+  const defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const result = await api.putUserPreferences(instance, { ianaTimezone: defaultTimezone });
+
+  if (result.isFailure) {
+    dispatch(preferencesRejected());
+
+    if (isAuthorizationError(result.error)) {
+      dispatch(authorizeRejected());
+    }
+
+    return;
+  }
+
+  const user = result.value();
+  dispatch(preferencesResolved(user));
+};
+
+export const {
+  userPending,
+  userResolved,
+  userRejected,
+  preferencesPending,
+  preferencesResolved,
+  preferencesRejected,
+} = userSlice.actions;
 export default userSlice.reducer;
