a78d2a11949b8335b96489c76afc4a40876e5d31
Eduardo Souza <edux404@gmail.com>
2020-04-15 04:02:15 -0300

    [ColoredMessageBox] Add submessage prop

--- src/app/components/ColoredMessageBox.tsx
+++ src/app/components/ColoredMessageBox.tsx
@@ -5,21 +5,32 @@ import { ThemeProvider, Paper, Typography } from '@material-ui/core';
 
 import { ColoredMuiTheme } from '../mui-theme';
 
+import { Stack } from './Stack';
+
 export interface ColoredMessageBoxProps {
   message: React.ReactNode;
+  submessage?: React.ReactNode;
 }
 
 export const ColoredMessageBox: React.FC<ColoredMessageBoxProps> = ({
   message,
+  submessage,
   children,
   ...other
 }) => {
   return (
     <ThemeProvider theme={ColoredMuiTheme}>
       <Paper css="padding: 16px" elevation={0} {...other}>
-        <Typography variant="h4" component="span" color="textPrimary">
-          {message}
-        </Typography>
+        <Stack spacing={4}>
+          <Typography variant="h4" component="span" color="textPrimary">
+            {message}
+          </Typography>
+          {submessage && (
+            <Typography variant="body1" component="span" color="textPrimary" css="opacity: 0.6">
+              {submessage}
+            </Typography>
+          )}
+        </Stack>
         {children}
       </Paper>
     </ThemeProvider>
