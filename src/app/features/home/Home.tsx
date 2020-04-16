854ae2cd27b26794f31443521d5633098d2f4611
Eduardo Souza <edux404@gmail.com>
2020-04-15 23:43:00 -0300

    [Home] Move weeks panel from return statement to separate component

--- src/app/features/home/Home.tsx
+++ src/app/features/home/Home.tsx
@@ -1,11 +1,9 @@
 import 'styled-components/macro';
 
 import React, { useState, useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useRouteMatch, Redirect, Switch, Route, useLocation } from 'react-router-dom';
 import { TransitionGroup } from 'react-transition-group';
-import { Typography, CircularProgress, Collapse, Button, Fade } from '@material-ui/core';
-import { format, parseJSON } from 'date-fns';
 
 import { AppState } from '@threecharts/app/redux/store';
 import { defaultClient, api } from '@threecharts/services/api';
@@ -13,16 +11,13 @@ import { useResizeObserver } from '@threecharts/hooks/useResizeObserver';
 import { useScrollDirection } from '@threecharts/hooks/useScrollDirection';
 import { TransitionSharedContainer } from '@threecharts/app/components/TransitionSharedContainer';
 import { TransitionFadeThrough } from '@threecharts/app/components/TransitionFadeThrough';
-import { ColoredMessageBox } from '@threecharts/app/components/ColoredMessageBox';
-import { Stack } from '@threecharts/app/components/Stack';
 import { ChartsDto } from '@threecharts/models/ChartsDto';
 
-import { WeeksPanel } from '../weeks';
-import { getWeeks, getOutdatedWeeks, syncWeeks, syncDismiss, selectWeek } from '../weeks/slice';
 import { ChartScreen } from '../charts/ChartScreen';
 import { UserProfile } from '../user/UserProfile';
 
 import { HomeBottomNavigation } from './HomeBottomNavigation';
+import { HomeWeeksPanel } from './HomeWeeksPanel';
 import { Styled } from './Home.styles';
 
 type AsyncStatus = 'idle' | 'pending' | 'resolved' | 'rejected';
@@ -33,9 +28,7 @@ export const Home = () => {
   const [charts, setCharts] = useState<ChartsDto | null>(null);
 
   const { currentUser: user } = useSelector((state: AppState) => state.user);
-  const { weeks, selectedWeekId, outdatedWeeks, status: weekStatus, syncStatus } = useSelector(
-    (state: AppState) => state.weeks,
-  );
+  const { selectedWeekId } = useSelector((state: AppState) => state.weeks);
 
   const [scrollHandler, scrollDirection] = useScrollDirection();
   const [navBarRef, navBarEntry] = useResizeObserver();
@@ -43,19 +36,6 @@ export const Home = () => {
   const location = useLocation();
   const indexMatch = useRouteMatch('/');
   const profileMatch = useRouteMatch('/profile');
-
-  const dispatch = useDispatch();
-
-  const dispatchSelectWeek = useCallback((weekId) => dispatch(selectWeek(weekId)), [dispatch]);
-
-  const fetchWeeks = useCallback(async () => {
-    if (user === null) {
-      return;
-    }
-
-    dispatch(getWeeks(defaultClient, user.id));
-    dispatch(getOutdatedWeeks(defaultClient, user.ianaTimezone));
-  }, [dispatch, user]);
 
   const fetchCharts = useCallback(async () => {
     if (user === null || !selectedWeekId) {
@@ -73,22 +53,6 @@ export const Home = () => {
     }
   }, [selectedWeekId, user]);
 
-  const trySyncWeeks = useCallback(() => {
-    if (user?.ianaTimezone === null) {
-      return;
-    }
-
-    dispatch(syncWeeks(defaultClient, user?.ianaTimezone ?? null));
-  }, [dispatch, user]);
-
-  const dismissSyncWeeks = useCallback(() => {
-    dispatch(syncDismiss());
-  }, [dispatch]);
-
-  useEffect(() => {
-    fetchWeeks();
-  }, [fetchWeeks]);
-
   useEffect(() => {
     fetchCharts();
   }, [fetchCharts]);
@@ -97,21 +61,9 @@ export const Home = () => {
     setIsWeeksPanelOpen(false);
   }, [selectedWeekId]);
 
-  const isSyncing = syncStatus === 'pending';
-  const didSyncFail = syncStatus === 'rejected';
-
-  const hasOutdatedWeeks = outdatedWeeks.count > 0 && !isSyncing;
-  const didWeeksFail = weekStatus === 'rejected' || outdatedWeeks.status === 'rejected';
-  const areWeeksLoading = weekStatus === 'pending' || outdatedWeeks.status === 'pending';
-
   const navBarHeight = navBarEntry?.contentRect.height ?? 56;
   const isNavBarHidden = scrollDirection === 'DOWN';
-
-  const selectedWeek = weeks.find((week) => week.id === selectedWeekId);
-  const weeksPanelBaseTitle = selectedWeek ? `Semana ${selectedWeek.weekNumber}` : 'Semanas';
-  const weeksPanelTitleWithReminder = hasOutdatedWeeks
-    ? `${weeksPanelBaseTitle} •`
-    : weeksPanelBaseTitle;
+  const isWeeksPanelHidden = isNavBarHidden || !!profileMatch;
 
   const commonChartScreenProps = {
     onRetry: fetchCharts,
@@ -170,113 +122,13 @@ export const Home = () => {
           </TransitionFadeThrough>
         </TransitionGroup>
       </Styled.HomeContent>
-      <Styled.HomeWeeksPanelContainer
-        windowHeight={window.innerHeight}
+      <HomeWeeksPanel
         navBarHeight={navBarHeight}
-        isHidden={isNavBarHidden || !!profileMatch}
+        isHidden={isWeeksPanelHidden}
         isOpen={isWeeksPanelOpen}
-        alwaysVisibleHeight={56}
-      >
-        <WeeksPanel
-          isOpen={isWeeksPanelOpen}
-          onOpen={() => setIsWeeksPanelOpen(true)}
-          onClose={() => setIsWeeksPanelOpen(false)}
-          onRefresh={fetchWeeks}
-          isLoading={areWeeksLoading}
-          value={selectedWeekId ?? null}
-          onChange={dispatchSelectWeek}
-          title={weeksPanelTitleWithReminder}
-          ContainerProps={{ elevation: 2 }}
-        >
-          <Collapse
-            in={areWeeksLoading || didWeeksFail || hasOutdatedWeeks || isSyncing || didSyncFail}
-          >
-            <Fade in={areWeeksLoading} unmountOnExit>
-              <Stack direction="row" justify="center" align="center" padding="16px 0" spacing={16}>
-                <CircularProgress color="inherit" size={16} />
-                <Typography color="textPrimary" variant="body1">
-                  Carregando semanas...
-                </Typography>
-              </Stack>
-            </Fade>
-            <Fade in={didWeeksFail} unmountOnExit>
-              <ColoredMessageBox
-                message="Algo deu errado. Verifique sua conexão e tente novamente."
-                css="margin: 16px"
-              >
-                <Stack direction="row" justify="flex-end" padding="32px 0 0">
-                  <Button color="primary" variant="contained" onClick={fetchWeeks}>
-                    Tentar Novamente
-                  </Button>
-                </Stack>
-              </ColoredMessageBox>
-            </Fade>
-            <Fade
-              in={hasOutdatedWeeks && !areWeeksLoading && !didWeeksFail && !didSyncFail}
-              unmountOnExit
-            >
-              <ColoredMessageBox
-                message={`Você tem ${outdatedWeeks.count} ${
-                  outdatedWeeks.count > 1
-                    ? 'semanas para serem sincronizadas'
-                    : 'semana para ser sincronizada'
-                }`}
-                css="margin: 16px"
-              >
-                <Stack direction="row" justify="flex-end" padding="32px 0 0">
-                  <Button color="primary" variant="contained" onClick={trySyncWeeks}>
-                    Sincronizar Agora
-                  </Button>
-                </Stack>
-              </ColoredMessageBox>
-            </Fade>
-            <Fade in={isSyncing} unmountOnExit>
-              <ColoredMessageBox
-                css="margin: 16px"
-                message={
-                  <Stack direction="row" align="center" spacing={16}>
-                    <CircularProgress color="inherit" size={16} />
-                    <span>Sincronizando semanas...</span>
-                  </Stack>
-                }
-                submessage="Isso pode demorar um pouco. Evite sair da página."
-              />
-            </Fade>
-            <Fade in={didSyncFail && !didWeeksFail} unmountOnExit>
-              <ColoredMessageBox
-                css="margin: 16px"
-                message="Algo deu errado ao sincronizar suas semanas."
-              >
-                <Stack
-                  direction="row"
-                  justify="flex-end"
-                  align="stretch"
-                  padding="32px 0 0"
-                  spacing={8}
-                >
-                  <Button color="primary" onClick={dismissSyncWeeks}>
-                    Fechar
-                  </Button>
-                  <Button color="primary" variant="contained" onClick={trySyncWeeks}>
-                    Tentar Novamente
-                  </Button>
-                </Stack>
-              </ColoredMessageBox>
-            </Fade>
-          </Collapse>
-          {[...weeks] // You can't run sort() on Redux arrays!
-            .sort((a, b) => b.weekNumber - a.weekNumber)
-            .map((week) => ({
-              id: week.id,
-              weekNumber: week.weekNumber,
-              from: format(parseJSON(week.from), 'dd/MM/yyyy'),
-              to: format(parseJSON(week.to), 'dd/MM/yyyy'),
-            }))
-            .map((week) => (
-              <WeeksPanel.Item key={week.id} data={week} />
-            ))}
-        </WeeksPanel>
-      </Styled.HomeWeeksPanelContainer>
+        onOpen={() => setIsWeeksPanelOpen(true)}
+        onClose={() => setIsWeeksPanelOpen(false)}
+      />
       <Styled.HomeNavigationBar
         ref={navBarRef}
         navBarHeight={navBarHeight}
