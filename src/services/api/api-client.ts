0aeae7a7ea53ef5a13f1fb6006a3ad8a6043be32
Eduardo Souza <edux404@gmail.com>
2020-04-23 15:08:43 -0300

    [ApiClient] Add support for API url in .env file

--- src/services/api/api-client.ts
+++ src/services/api/api-client.ts
@@ -2,5 +2,5 @@ import axios, { AxiosInstance } from 'axios';
 
 export const instance: AxiosInstance = axios.create({
   withCredentials: true,
-  baseURL: 'http://192.168.1.165:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
